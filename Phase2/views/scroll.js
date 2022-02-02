837ea62c72e61527df7faff8f1ea931e1954463c
Naol Tamrat <naoltamrat11@gmail.com>
2022-02-02 08:25:54 +0300

    phase 2

--- Phase2/views/scroll.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('a[href^="#Services"]').forEach(anchor => {
-    anchor.addEventListener("click", function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior: "smooth"
-        });
-
-    });
-});
